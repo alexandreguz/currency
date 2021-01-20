81645a2f0847cf4c1c5afcf06e966ad7c02a97ce
Alexandre <alexandre@weel.com>
2021-01-20 13:25:04 +0200

    collapse with ajax call for the coins price

--- main.js
+++ main.js
@@ -1,76 +1,65 @@
-document.getElementById('quote').addEventListener("click", callAjax)
+var coinsList;
+var coinsPrices;
+var coinID;
+var coinMoreInfo;
 
-
-$(document).ready(function () {
-    callAjax();
-    callMoreInfoAjax();
+$(function () {
+    getAllCoins();
 
 })
 
-var ajax_arr;
-
-function callAjax() {
+function getAllCoins() {
     $.ajax({
-        // url: "https://api.coingecko.com/api/v3/coins/list",
         url: "https://api.coingecko.com/api/v3/coins/list",
-        // url: "coins_list2.json",
-
         dataType: "json",
         crossDomain: "true",
-        success: function (data) {
-            ajax_arr = data;
+        success: function (data) { 
+            coinsList = data;
+
+            for (let i = 0; i < 100; i++) {
+                result =
+
+                    "<div class='card' style='width: 18rem;'>" +
+                        "<div class='card-body'>" +
+                            "<div class='row'>" +
+                                "<h5 class='card-title col-8'>" + coinsList[i].symbol + "</h5>" + "<br>" +
+                                "<label class='switch'>" +
+                                "<input type='checkbox'>" +
+                                "<span class='slider round'>"+"</span>" +
+                                "</label>" +
+                            "</div>" +
+                                "<h6 class='card-subtitle mb-2 text-muted'>" + coinsList[i].name + "</h6>" + "<br>" +
+                                "<button onclick='getMoreInfoForCoin(`"+ coinsList[i].id + "`)' class='btn btn-primary' data-toggle='collapse' data-target='#" + coinsList[i].id + "'>" +
+                                "More Info" + 
+                                "</button>" +
+                            "<div id='" + coinsList[i].id + "'class='collapse'>" +  // problem is the ID
+                                 
+                            "</div>"
+                        "</div>"
+                    "</div>"
+
+                document.getElementById("row-card").innerHTML += result;
+            }
         }
     })
-
-    for (let i = 0; i < 100; i++) {
-        result =
-            "<div class='card' style='width: 18rem;'>" +
-                "<div class='card-body'>" +
-                    "<div class='row'>" +
-                        "<h5 class='card-title col-8'>" + ajax_arr[i].symbol + "</h5>" + "<br>" +
-                        "<label class='switch'>" +
-                        "<input type='checkbox'>" +
-                        "<span class='slider round'>"+"</span>" +
-                        "</label>" +
-                    "</div>" +
-                    "<h6 class='card-subtitle mb-2 text-muted'>" + ajax_arr[i].name + "</h6>" + "<br>" +
-                        "<button onclick='callMoreInfoAjax()' class='btn btn-primary' data-toggle='collapse' data-target='#demo" + [i] + "'>" +
-                        "More Info" + 
-                        "</button>" +
-                    "<div id='demo" + [i] + "'class='collapse'>" +
-                        "Lorem ipsum dolor sit amet, consectetur adipisicing " +
-                    "</div>"
-                "</div>"
-            "</div>"
-
-        document.getElementById("row-card").innerHTML += result;
-    }
 }
 
-var ajax_info_arr;
-function callMoreInfoAjax(i) {
-
-
-    $.ajax({
-        url: `https://api.coingecko.com/api/v3/coins/${ajax_arr[i].id}`,
+function getMoreInfoForCoin(coinID) { 
+    $.ajax({ 
+        url: `https://api.coingecko.com/api/v3/coins/${coinID}`,
         dataType: "json",
         crossDomain: "true",
-        success: function (data) {
-            ajax_info_arr = data;
+        success: function (coinMoreInfo) { debugger;
+
+                     document.getElementById(coinID).innerHTML =
+            
+                    `USD ${coinMoreInfo.market_data.current_price.usd} <br>
+                     EUR ${coinMoreInfo.market_data.current_price.eur} <br>
+                     ILS ${coinMoreInfo.market_data.current_price.ils} <br>` 
+
+                    $("#" + coinID).collapse('toggle')
+
         }
     })
-    for (let i = 0; i < 100; i++) {
-
-        result2 =
-            " <div id='demo" + [i] + "' class='collapse'>" +
-            `${market_data.current_price.usd} <br>
-             ${market_data.current_price.eur} <br>
-             ${market_data.current_price.ils} <br>` +
-            " </div>"
-
-
-
-    }
-
 }
 
