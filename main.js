918ddf16b629a07b4f01b2b9ddb71b6beb3c1cb3
Alexandre <alexandre@weel.com>
2021-01-27 14:43:03 +0200

    modal partialy working with coins

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ var coinsList;
 var coinsPrices;
 var coinID;
 var coinMoreInfo;
-var values = []
+var clickedCoins = []
+var coinsFromApi = []
 
 $(function () {
     getAllCoins();
-
-
 })
 
 function getAllCoins() {
@@ -16,56 +15,67 @@ function getAllCoins() {
         dataType: "json",
         crossDomain: "true",
         success: function (data) { 
-            coinsList = data;
+            coinsList = data.map(coin => {return {...coin, checked: false}});
+            renderCoins(coinsList);
 
-            for (let i = 0; i < 100; i++) {
-                result =
+            $("input").on("click", function () {
+                var id = $(this).attr("value");
+                if ($(this).is(":checked")) {
+                
+                    var clickedCoin = coinsList.find(coin => coin.id == id)
 
-                    "<div class='card' style='width: 18rem;'>" +
-                        "<div class='card-body'>" +
-                            "<div class='row'>" +
-                                "<h5 class='card-title col-8'>" + coinsList[i].symbol + "</h5>" + "<br>" +
-                                "<label class='switch'>" +
-                                "<input type='checkbox' id='checkId"+[i]+"'name='coin' value='coin"+[i]+"' >" +
-                                "<span class='slider round'>"+"</span>" +
-                                "</label>" +
-                            "</div>" +
-                                "<h6 class='card-subtitle mb-2 text-muted'>" + coinsList[i].name + "</h6>" + "<br>" +
-                                "<button onclick='getMoreInfoForCoin(`"+ coinsList[i].id + "`)' class='btn btn-primary' data-toggle='collapse' data-target='#" + coinsList[i].id + "'>" +
-                                "More Info" + 
-                                "</button>" +
-                            "<div id='" + coinsList[i].id + "'class='collapse'>" +  
-                                 
-                            "</div>"
-                        "</div>"
-                    "</div>"
-
-                document.getElementById("row-card").innerHTML += result;
-
-
-                $("input").on("click", function () {
-                    var id = $(this).attr("id");
-                    if ($(this).is(":checked")) {
-                 
-                      values.push(id);
-                      if (values.length > 5) {
-                        var mymodal = $("#exampleModal");
-                        mymodal.find('.modal-body').html(`<label class='switch'>
-                        <input type='checkbox' id='${values[5]}' name='coin' value='${values[5]}' >
+                    clickedCoins.push(clickedCoin);
+                    coinsList = coinsList.map(coin => {return coin.id == id ? {...coin, checked: true} : coin})
+                    if (coinsList.filter(c=> c.checked).length > 5) { 
+                        var modalHtml = clickedCoins.map(coin => {return `<div><div class='row'><h5 class='col-8'>${coin.symbol}</h5>
+                        <label class='switch'> 
+                        <input type='checkbox' id='${coin.id}' name='coin' class='' checked onclick='unclickFromModal("${coin.id}")'>
                         <span class='slider round'></span>" 
-                        "</label>`); // add here the coins
-                        mymodal.modal('show');
-
-                      }
+                        "</label></div></div>`})
+                    var mymodal = $("#exampleModal");
+                    mymodal.find('.modal-body').html(modalHtml.join('')); 
+                    mymodal.modal('show');
                     }
-                    else {
-                      values.splice(values.indexOf(id), 1);
-                    }
-                  });
-
-            }
+                }
+                else {
+                    clickedCoins.splice(clickedCoins.indexOf(id), 1);
+                }
+            });
         }
     })
+}
+
+function unclickFromModal(coinId) {
+    //descheckar essa coin
+    coinsList = coinsList.map(coin => {return coin.id == coinId ? {...coin, checked: false} : coin})
+    //re-renderizar coins
+    renderCoins(coinsList);
+    //fechar modal
+    $("#exampleModal").modal('hide');
+}
+
+function renderCoins(coinsList) {
+    document.getElementById("row-card").innerHTML = ""
+    for (let i = 0; i < 100; i++) {
+        result = `<div class='card' style='width: 18rem;'>
+        <div class='card-body'>
+            <div class='row'>
+                <h5 class='card-title col-8'> ${coinsList[i].symbol} </h5><br>
+                <label class='switch'>
+                <input type='checkbox' id='checkId${[i]}' name='coin' value='${coinsList[i].id}' ${coinsList[i].checked ? 'checked' : ''}>
+                <span class='slider round'></span>
+                </label>
+            </div>
+                <h6 class='card-subtitle mb-2 text-muted'> ${coinsList[i].name}</h6><br>
+                <button onclick='getMoreInfoForCoin("${coinsList[i].id}")' class='btn btn-primary' data-toggle='collapse' data-target='#${coinsList[i].id}'>
+                "More Info" 
+                </button>
+            <div id='${coinsList[i].id}'class='collapse'></div>
+        </div>
+    </div>`
+
+    document.getElementById("row-card").innerHTML += result;
+    }
 }
 
 function getMoreInfoForCoin(coinID) { 
@@ -73,7 +83,7 @@ function getMoreInfoForCoin(coinID) {
         url: `https://api.coingecko.com/api/v3/coins/${coinID}`,
         dataType: "json",
         crossDomain: "true",
-        success: function (coinMoreInfo) { debugger;
+        success: function (coinMoreInfo) { 
 
                      document.getElementById(coinID).innerHTML =
             
@@ -86,3 +96,7 @@ function getMoreInfoForCoin(coinID) {
         }
     })
 }
+
+
+
+
